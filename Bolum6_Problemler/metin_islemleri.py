a90e401b4f86aa14ffd45f66d006dd1492483c48
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2020-08-06 15:23:41 +0300

    Yeni örnekler eklendi.

---
+++ Bolum6_Problemler/metin_islemleri.py
@@ -0,0 +1,46 @@
+import os
+
+path = os.getcwd()
+class Dosya():
+
+    def __init__(self):
+        with open(path +"/Bolum6_Problemler/metin.txt","r",encoding="utf-8") as file:
+            dosya_icerigi = file.read()
+
+            kelimeler = dosya_icerigi.split()
+            self.sade_kelimeler = list()
+        
+            for i in kelimeler:
+                i = i.strip("\n")
+
+                i = i.strip(" ")
+                i = i.strip(".")
+                i = i.strip(",")
+                self.sade_kelimeler.append(i)
+    
+    def tum_kelimeler(self):
+        kelimeler_kumesi = set()
+        for i in self.sade_kelimeler:
+            kelimeler_kumesi.add(i)
+
+        print("Tüm kelimeler : ")
+        for i in kelimeler_kumesi:
+            print(i)
+            print("**************")
+    
+    def kelime_frekansi(self):
+        kelime_sozluk = dict()
+        for i in self.sade_kelimeler:
+            if(i in kelime_sozluk):
+                kelime_sozluk[i] += 1
+            
+            else:
+                kelime_sozluk[i] = 1
+        
+        for kelime,sayi in kelime_sozluk.items():
+            print(" {} kelimesi {} defa geçiyor. ".format(kelime,sayi))
+            print("----------")
+
+dosya = Dosya()
+dosya.kelime_frekansi()
+
---
+++ Bolum7_Problemler/decorator_ornegi.py
@@ -0,0 +1,31 @@
+def ekstra(fonksiyon):
+    def wrapper(sayilar):
+        ciftler_toplami = 0
+        cift_sayilar = 0
+        tekler_toplami = 0
+        tek_sayilar = 0
+
+        for sayi in sayilar:
+
+            if sayi % 2 == 0 :
+                ciftler_toplami += sayi
+                cift_sayilar += 1
+            
+            else:
+                tekler_toplami += sayi
+                tek_sayilar += 1
+        print("Teklerin ortalaması : ", tekler_toplami/tek_sayilar)
+        print("Çiftlerin ortalaması : ",ciftler_toplami/cift_sayilar)
+        
+        fonksiyon(sayilar)
+    
+    return wrapper
+
+@ekstra
+def ortalama_bul(sayilar):
+    toplam = 0
+    for sayi in sayilar:
+        toplam += sayi
+    print("Genel Ortalama : ", toplam/len(sayilar))
+
+ortalama_bul([1,2,3,4,5,6])
